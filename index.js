5d260b31e6f2a40d4b7e855f57a7baa3d80dd396
Kenessary <33985287+IsOnlineUser@users.noreply.github.com>
2021-08-27 22:00:19 -0500

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const mongo = require("mongodb").MongoClient;
 
-io.on('connection', socket => {
-  socket.on('message', ({ name, message }) => {
-    io.emit('message', { name, message })
-  })
-})
+const PORT = process.env.PORT || 4000;
 
-http.listen(4000, function() {
-  console.log('listening on port 4000')
-})
+// Connect to mongo
+mongo.connect(
+  "mongodb+srv://admin:admin123@cluster0.mnx35.mongodb.net/mongochat?retryWrites=true&w=majority",
+  function (err, client) {
+    if (err) {
+      throw err;
+    }
+let db = client.db('chats')
+    console.log("MongoDB connected...");
+
+    io.on("connection", (socket) => {
+      let chat = db.collection("chats");
+
+      // Create function to send status
+      sendStatus = function (s) {
+        socket.emit("status", s);
+      };
+      socket.on("message", ({ name, message }) => {
+        io.emit("message", { name, message });
+      });
+
+      socket.on("room", (room) => {
+        io.emit("room", room);
+      });
+    });
+
+    http.listen(PORT, function () {
+      console.log(`listening on port ${PORT}`);
+    });
+  }
+);
